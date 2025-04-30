c7a45a25a922deb0721c6e6fad02a2767cb5609b
Akmal Avloni <akmal.avloni86@gmail.com>
2025-04-30 14:04:29 -0700

    3.05 isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,32 +20,15 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// Initialize a new instance of our application struct, containing the
-	// dependencies (for now, just the structured logger).
 	app := &application{
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// Register the other application routes as normal..
-	mux.HandleFunc("GET /", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	// Create the new route, which is restricted to POST requests only.
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-	// Use the Info() method to log the starting server message at Info severity
-	// (along with the listen address as an attribute).
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
-	// And we also use the Error() method to log any error message returned by
-	// http.ListenAndServe() at Error severity (with no additional attributes),
-	// and then call os.Exit(1) to terminate the application with exit code 1.
+	// Call the new app.routes() method to get the servemux containing our routes,
+	// and pass that to http.ListenAndServe().
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
